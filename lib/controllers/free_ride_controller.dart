ec196a2ea264afe0963971b2dacc1e1132d434b6
magnus.coder <kelimbth@gmail.com>
2025-05-04 13:16:42 +0300

    feat: updated `FreeRideController()`

--- lib/controllers/free_ride_controller.dart
+++ lib/controllers/free_ride_controller.dart
@@ -1,10 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
 import '../models/user.dart';
+import '../providers/user.dart';
 import '../services/user.dart';
 
 class FreeRideController {
   final UserServices _userServices = UserServices();
 
-  Future<void> deductFreeRide({required UserModel userModel, required double rideCost}) async {
+  Future<void> deductFreeRide({required BuildContext context, required UserModel userModel, required double rideCost}) async {
     if (userModel.freeRidesRemaining > 0 && userModel.freeRideAmountRemaining > 0) {
       double remaining = userModel.freeRideAmountRemaining - rideCost;
 
@@ -20,6 +24,9 @@ class FreeRideController {
       userModel
         ..freeRidesRemaining = newFreeRides
         ..freeRideAmountRemaining = finalRemaining;
+      
+       // Update UI state
+      Provider.of<UserProvider>(context, listen: false).updateFreeRides(newFreeRides, finalRemaining);
     }
   }
 
