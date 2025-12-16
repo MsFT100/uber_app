ad5ef12c35a99e3d3c31fb4b18b45a2be9ccd93b
MsFT <lennoxmathew@gmail.com>
2025-12-16 16:41:33 +0300

    feat; optimized dimensions. -

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -18,7 +18,6 @@ class AppConfig {
     }
     if (Platform.isAndroid) {
       //return 'http://10.0.2.2:3001'; // Use 10.0.2.2 for Android emulator
-      //return 'http://YOUR_COMPUTER_IP:3001'; // IMPORTANT: Replace with your computer's actual IP address
       return 'http://10.65.205.247:3001'; // Use your computer's local IP for physical devices
     }
     // For iOS simulator, macOS, etc.
